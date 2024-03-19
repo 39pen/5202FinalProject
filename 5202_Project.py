6a9a06972671d850b36267dd341da4c4a3dcdd2a
39pen <93582904+39pen@users.noreply.github.com>
2024-03-19 14:26:38 +0800

    Update 5202_Project.py

--- 5202_Project.py
+++ 5202_Project.py
@@ -10,19 +10,16 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import io
 import base64
+from datetime import datetime
 import tabulate
 import plotly.express as px
 
 app = dash.Dash(__name__)
 
-# Generate dropdown options for hours (0-23) and minutes (0-59)
-hour_options = [{'label': str(i).zfill(2), 'value': str(i).zfill(2)} for i in range(24)]
-minute_options = [{'label': str(i).zfill(2), 'value': str(i).zfill(2)} for i in range(60)]
-
 
 # Layout of the dashboard
 app.layout = html.Div(children=[
-    html.H1(children='My Data Dashboard',
+    html.H1(children='Games Dashboard',
             style={
             'textAlign': 'center'
             }
@@ -32,154 +29,107 @@ app.layout = html.Div(children=[
     dcc.Upload(
         id='upload-data',
         children=html.Button('Upload File'),
-        multiple=False
+        multiple=False # Restrict to single file upload
     ),
 
+    dcc.DatePickerRange(
+        id='date-picker-range',
+        display_format='YYYY-MM-DD',
+        start_date='1900-01-01',  # Set default start date to 1900-01-01
+        end_date=datetime.today().strftime('%Y-%m-%d'),  # Set default end date to today's date
+        style={'margin': '20px 0'}
+    ),
+    
     # Text to display summary
     html.Div(id='data-summary-text',
              style={'width': '100%'}),
 
-    # Times-series analysis
-    html.Div([
-        dcc.DatePickerRange(
-            id='date-picker-range',
-            display_format='MM/DD/YYYY',  # Include date only
-            start_date=pd.to_datetime('12/01/2010').strftime('%Y-%m-%d'),  # Set default to 12/01/2010
-            #start_date=pd.to_datetime('today').strftime('%Y-%m-%d'),  # Set default to today's date
-            end_date=pd.to_datetime('today').strftime('%Y-%m-%d'),  # Set default to today's date
-            style={'width': '50%'}  # Set the width to 50%
-        ),
-        html.Label('Select Time Range:'),
-        dcc.RangeSlider(
-            id='time-range-slider',
-            marks={i: {'label': str(i).zfill(2)} for i in range(24)},
-            min=0,
-            max=23,
-            step=1,
-            value=[0, 23]
-        ),
-    ], style={'width': '80%', 'margin': 'auto'}),  # Set an explicit width and center the date picker
-
-    # Pie chart
-    dcc.Graph(id='pie-chart'),
-
-    # Bar graph
-    dcc.Graph(id='bar-graph'),
-    
-    # Text to display
-    html.Div(id='text-to-display',
-             style={'width': '100%'}),
-
-    # Display filtered_df
-    html.Div(id='filtered-data-display',
-             style={'width': '100%', 'whiteSpace': 'pre-line'}),
+    # Containers for the six metrics
+    html.Div(id='metrics-container', style={'display': 'flex', 'justifyContent': 'space-around', 'flexWrap': 'wrap', 'gap': '20px'}, children=[
+        html.Div(children=[
+            html.H4("Number of Games"),
+            html.Div(id='num-games', className='metric-box')
+        ], className='metric-container'),
+        
+        html.Div(children=[
+            html.H4("Average Plays"),
+            html.Div(id='avg-plays', className='metric-box')
+        ], className='metric-container'),
+        
+        html.Div(children=[
+            html.H4("Average Rating"),
+            html.Div(id='avg-rating', className='metric-box')
+        ], className='metric-container'),
+        
+        html.Div(children=[
+            html.H4("Number of Descriptions"),
+            html.Div(id='num-description', className='metric-box')
+        ], className='metric-container')
+    ])
 
 ])
+
+# Add the below into containers section when data combined
+"""
+        html.Div(children=[
+            html.H4("Number of Genres"),
+            html.Div(id='num-genres', className='metric-box')
+        ], className='metric-container'),
+
+        html.Div(children=[
+            html.H4("Number of Developers"),
+            html.Div(id='unique-developers', className='metric-box')
+        ], className='metric-container'),
+"""
 
 
 # Callback to update the graph based on the uploaded file
 @app.callback(
-    Output('data-summary-text', 'children'),
-    Output('text-to-display', 'children'),
-    Output('filtered-data-display', 'children'),
-    Output('pie-chart', 'figure'),
-    Output('bar-graph', 'figure'),
-    Input('upload-data', 'contents'),
-    Input('date-picker-range', 'start_date'),
-    Input('date-picker-range', 'end_date'),
-    Input('time-range-slider', 'value'),
+    [Output('data-summary-text', 'children'),
+     Output('num-games', 'children'),
+     #Output('num-genres', 'children'),
+     #Output('unique-developers', 'children'),
+     Output('avg-plays', 'children'),
+     Output('avg-rating', 'children'),
+     Output('num-description', 'children')],
+    [Input('upload-data', 'contents'),
+     Input('upload-data', 'filename'),
+     Input('date-picker-range', 'start_date'),
+     Input('date-picker-range', 'end_date')]
 )
 
-def update_summary(contents, start_date, end_date, selected_time_range):
-    
-    if contents is None:
-        return 'Upload a file to see the summary.', "", "", px.pie(), px.bar()
+def update_summary(content, filename, start_date, end_date):
 
-    # Read the uploaded CSV file
-    content_type, content_string = contents.split(',')
-    decoded = io.StringIO(base64.b64decode(content_string).decode('latin1'))
+    # Initial placeholders for the statistics
+    num_games = avg_plays = avg_rating = num_description = 'N/A'
+    # When data combined, use: num_games = num_genres = unique_developers = avg_plays = avg_rating = num_description = 'N/A'
+    
+    if content is None:
+        return 'Upload a file to see the summary.', num_games, avg_plays, avg_rating, num_description
+        # When data combined, use: return 'Upload a file to see the summary.', num_games, num_genres, unique_developers, avg_plays, avg_rating, num_description
+
+    # Decode the uploaded file
+    content_type, content_string = content.split(',')
+    decoded = io.StringIO(base64.b64decode(content_string).decode('utf-8'))
     df = pd.read_csv(decoded)
+    # Give a summary of the file
+    summary_text = f'{filename} contains {len(df)} rows.'
 
-    # Convert 'InvoiceDate' to datetime format
-    df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'], format='%m/%d/%Y %H:%M', errors='coerce')
+    # Filter according to date selection
+    df['date'] = pd.to_datetime(df['date'], errors='coerce')
+    filtered_df = df[(df['date'] >= pd.to_datetime(start_date)) & (df['date'] <= pd.to_datetime(end_date))]
+    # Overview of data within the selected date range
+    num_games = len(filtered_df)
+    #num_genres = filtered_df['genre'].nunique()
+    #unique_developers = filtered_df['developer'].nunique()
+    avg_plays = round(filtered_df['plays'].mean(), 2)
+    avg_rating = round(filtered_df['rating'].mean(), 2)
+    num_description = filtered_df['description'].notna().sum()
+
     
-    # Convert selected start and end dates along with hours and minutes to pandas Timestamp objects
-    start_datetime = pd.to_datetime(start_date, format='%Y-%m-%d', errors='coerce') + pd.to_timedelta(selected_time_range[0], unit='h')
-    end_datetime = pd.to_datetime(end_date, format='%Y-%m-%d', errors='coerce') + pd.to_timedelta(selected_time_range[-1], unit='h')
+    return summary_text, num_games, avg_plays, avg_rating, num_description
+    # When data combined, use: return summary_text, num_games, num_genres, unique_developers, avg_plays, avg_rating, num_description
 
-    date_msg = f"From {start_datetime} to {end_datetime}."
-    
-    # Filter data based on selected date range
-    filtered_df = df[(df['InvoiceDate'] >= start_datetime) & (df['InvoiceDate'] <= end_datetime)]
-
-    # Get summary information
-    num_rows = len(df)
-    summary_msg = f"The uploaded file contains {num_rows} rows."
-    filtered_rows = len(filtered_df)
-    filtered_msg = f"The selected date range contains {filtered_rows} rows."
-    final_msg = summary_msg + '\n' +filtered_msg
-
-    # Display filtered_df as text in html.Div
-    filtered_df_display = html.Div([
-        html.Hr(),
-        html.H4("Filtered Data:"),
-        dcc.Markdown(f"Number of rows: {filtered_rows}"),
-        dcc.Markdown(filtered_df.to_markdown())
-    ])
-
-    # Calculate percentage distribution of countries within the filtered DataFrame
-    country_percentages = (filtered_df['Country'].value_counts() / filtered_rows) * 100
-    country_msg = f"{country_percentages}."
-
-    # Check if the filtered DataFrame is not empty
-    if filtered_rows > 0:
-        # Create a pie chart using plotly express
-        pie_chart = px.pie(names=country_percentages.index, values=country_percentages, title='Distribution by Country')
-
-        # Create a bar graph using plotly express
-        bar_graph = px.bar(
-            x=filtered_df['Country'].value_counts().index,
-            y=filtered_df['Country'].value_counts().values,
-            labels={'x': 'Country', 'y': 'Frequency'},
-            title='Frequency of Each Country'
-        )
-
-        # Adjust the width of the bars in the bar graph
-        bar_graph.update_layout(bargap=0.8)
-        
-    else:
-        # If the filtered DataFrame is empty, create an empty pie chart and bar graph
-        pie_chart = px.pie(title='Distribution by Country')
-        bar_graph = px.bar(title='Frequency of Each Country')
-    
-    return final_msg, date_msg, filtered_df_display, pie_chart, bar_graph
-
-
-    """
-    decoded = None
-    if 'csv' in filename:
-        # Decode CSV file
-        decoded = pd.read_csv(StringIO(content_string))
-    elif 'xls' in filename:
-        # Decode Excel file
-        decoded = pd.read_excel(BytesIO(content_string))
-
-    print(f'Decoded DataFrame: {decoded}')
-        
-    if decoded is None or decoded.empty:
-        return f'Invalid or empty file. Upload a valid file. Decoded: {decoded}'
-
-    print('File read.')
-    
-    
-    # Calculate total sales per country
-    total_sales_per_country = decoded.groupby('Country')['Quantity'].sum()
-
-    # Create a summary text
-    summary_text = f'Total Sales per Country:\n\n{total_sales_per_country}'
-
-    return summary_text
-    """
     
 if __name__ == '__main__':
     app.run_server(debug=True)
